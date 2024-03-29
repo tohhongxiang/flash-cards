ffac17eb6326d7e514fdf679907e5918678e5280
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-03-29 16:10:15 +0800

    chore: Refactor validate-card to be consistent with validate-deck

--- features/card/create-card.ts
+++ features/card/create-card.ts
@@ -3,10 +3,7 @@ import validateCard from "./utils/validate-card";
 import { cardDb } from "@/services/db/actions";
 
 export default async function createCard(newCard: CreatedCardType) {
-    const error = validateCard(newCard);
-    if (error) {
-        throw new Error(error);
-    }
+    validateCard(newCard);
 
     const createdCard = cardDb.createCard(newCard);
     return createdCard;
--- features/card/utils/validate-card.ts
+++ features/card/utils/validate-card.ts
@@ -2,11 +2,11 @@ import { CreatedCardType } from "@/types/cardType";
 
 export default function validateCard(card: CreatedCardType) {
     if (card.front.length === 0) {
-        return "Card Front must not be empty!";
+        throw new Error("Card Front must not be empty!");
     }
 
     if (card.back.length === 0) {
-        return "Card Back must not be empty!";
+        throw new Error("Card Back must not be empty!");
     }
 
     return null;
