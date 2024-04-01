953e9127eaf06f094bea363fcc370941659f7290
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-04-01 17:57:42 +0800

    fix: Prevent creating new connections when hot reload

--- services/db/drizzle.ts
+++ services/db/drizzle.ts
@@ -1,9 +1,21 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 import * as schema from "./schema";
+import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
-const db = drizzle(client, { schema });
+declare global {
+    // eslint-disable-next-line no-var -- only var works here
+    var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let db: PostgresJsDatabase<typeof schema>;
+
+if (process.env.NODE_ENV === "production") {
+    db = drizzle(postgres(process.env.DATABASE_URL!), { schema });
+} else {
+    if (!global.db) {
+        global.db = drizzle(postgres(process.env.DATABASE_URL!), { schema });
+    }
+    db = global.db;
+}
 
 export default db;
